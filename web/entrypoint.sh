81663d3d30adb5ffe32536e75ad613338ad3fef0
Lars Kellogg-Stedman <lars@oddbit.com>
2019-04-05 08:18:59 -0400

    initial commit

---
+++ web/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir -p /srv/web
+cat <<EOF > /srv/web/index.html
+<html>
+<head>
+<title>$(hostname)</title>
+</head>
+<body>
+<h1>$(hostname)</h1>
+<h2>Network configuration</h2>
+<pre>
+$(ip addr show)
+</pre>
+<h2>Environment</h2>
+<pre>
+$(env)
+</pre>
+</body>
+</html>
+EOF
+
+exec "$@"
